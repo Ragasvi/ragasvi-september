64b48e322e444f5df1b7cef05e58135483c08f8e
Ragasvi <113579569+Ragasvi@users.noreply.github.com>
2022-09-15 19:18:52 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+#LOOPS
+
+
+#WHILE loop
+
+number = int(input("enter the number = "))
+total = 0
+while number <= 10:
+    total = total + number
+    number = number + 1
+print("value of total from the while loop is : ",total)    
+
