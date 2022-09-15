f06e7a4c3405b8f439dce416c4c0631e445e9c0c
Ragasvi <113579569+Ragasvi@users.noreply.github.com>
2022-09-15 19:08:02 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,9 @@
+#for loop is used to iterate over a sequenceehich would be in list,tuple,string
+
+X= ["language","javascript","oracle"]
+for i in X:
+    print(i)
+
+Y = "python"
+for i in Y:
+    print(i)
