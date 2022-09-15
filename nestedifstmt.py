8513fe0fc47eba5814526ec68190cec0a57d9111
Ragasvi <113579569+Ragasvi@users.noreply.github.com>
2022-09-15 11:50:49 +0530

    Add files via upload

---
+++ nestedifstmt.py
@@ -0,0 +1,18 @@
+#NESTED IF STATEMENT
+
+
+
+
+#c=78
+#c=20
+c=21
+if c <25:
+    if c%2 == 0:
+        print("c is an even number less than 25")
+    else:
+        print("c is an odd number less than 25")
+else:
+    print("c is greater than 25")
+    
+        
+        
---
+++ python numbers.py
@@ -0,0 +1,35 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #PYTHON NUMBERS
+>>> #INTEGERS
+>>> 
+>>> 
+>>> num=5
+>>> print(num)
+5
+>>> type(num)
+<class 'int'>
+>>> 
+>>> 
+>>> #FLOAT
+>>> 
+>>> num=9.89
+>>> print(num)
+9.89
+>>> type(num)
+<class 'float'>
+>>> 
+>>> 
+>>> 
+>>> #complex
+>>> 
+>>> 
+>>> num = 2+7j
+>>> print(num)
+(2+7j)
+>>> type(num)
+<class 'complex'>
+>>> 
+>>> 
+>>> print(complex(4,5))
+(4+5j)
