5c8a1409cf94b48cf7b7b0e94b777f765ed9b86c
Ragasvi <113579569+Ragasvi@users.noreply.github.com>
2022-09-15 19:42:56 +0530

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,11 @@
+# continue statement
+
+
+number = int(input("enter the value = "))
+while number > 0:
+    number = number - 1
+    if number == 2:
+        continue
+    print(number)
+print("loop end")    
+    
